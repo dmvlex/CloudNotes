4f0da604537e7d4f8b0ee13d28555598cee30492
Doomov <alexkharitonov.unvml@gmail.com>
2022-04-08 16:46:52 +0300

    First Commit
    Добавлены файлы проекта в репозиторию

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CloudNotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+
+        private void SettingsButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SynchronizationButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FilesDrop(object sender, DragEventArgs e)
+        {
+            //Получение и вывод всех путей к дропнутым файлам
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                foreach (var item in files)
+                {
+                    PathsOutput.Text += $"{item} \n";
+                }
+            }
+
+
+        }
+    }
+}
